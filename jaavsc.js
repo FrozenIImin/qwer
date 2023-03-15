43efae0b6f05cc7740fb4626e2c5547eca88fe83
FrozenIImin <127924316+FrozenIImin@users.noreply.github.com>
2023-03-15 15:40:28 +0900

    Add files via upload

---
+++ jaavsc.js
@@ -0,0 +1,17 @@
+    var myButton = document.getElementById('hw');
+
+        myButton.addEventListener('click', function() {
+            alert('Hello, World!');
+        });
+
+    var myButton2 = document.getElementById('qw');
+
+        myButton2.addEventListener('click', function() {
+            alert('Hello, World!');
+        });
+
+    var myButton3 = document.getElementById('er');
+
+        myButton3.addEventListener('click', function() {
+            alert('Hello, World!');
+        });
